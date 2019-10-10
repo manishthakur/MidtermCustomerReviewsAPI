c0038ec809abe4e3220bcd98dba4a324ad7722ba
neelamthakur.in@gmail.com <neelamthakur.in@gmail.com>
2019-10-09 22:35:59 -0400

    Updated code for second phase of project.

--- src/main/java/com/udacity/course3/reviews/repository/CommentMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udacity.course3.reviews.repository;
-
-import com.udacity.course3.reviews.entity.CommentDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CommentMongoRepository extends MongoRepository<CommentDocument, String> {
-}
